fd6476516fba580c01cc8ba9377a9d8c1cee2380
Sajed Ahmed Khan <sajedaiub@gmail.com>
2019-01-22 21:13:05 +0600

    some changes done

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
-a="sajed ahmed khan"
-print(a[::-1])
-b=8+9
-print(b)
+player = 'Thomas'
+points = 33
+k=" Last night, "+player+" scored "+str(points)+" points. "
+print(f'Last night, {player} scored {points} points.')  # concatenation
+
+a,b,c=3,4.7,'\nsajed\n'
+
+print("I'm going to inject %s here by %d to %s." %(a,b,c))
+
+print(repr(player))
+
+
+print('Floating point numbers: %2.1f' %(2.175909))
+
+print('may name is %s and my age is %d \n my weight is %2.5f thanks for you info %r' %('sajed',32,73.52,'bye bye!!!'))
+
+
+
